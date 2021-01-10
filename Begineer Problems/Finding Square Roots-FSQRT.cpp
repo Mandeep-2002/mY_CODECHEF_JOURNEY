fba92011bf0580dde5a2bc774b7876fc3ea05144
Mandeep Andey <51310663+Mandeep-2002@users.noreply.github.com>
2021-01-10 17:13:48 +0530

    Create Finding Square Roots-FSQRT.cpp

---
+++ Begineer Problems/Finding Square Roots-FSQRT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x;
+        cin >> x;
+        cout << floor(sqrt(x)) << "\n";
+    }
+	// your code goes here
+	return 0;
+}
